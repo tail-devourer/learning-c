4fb751e8f837f69566cc206387218526e4822cee
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2026-01-26 19:31:36 -0600

    Added a more complex example where the relation between arrays and pointers is shown.

--- pointers.c
+++ pointers.c
@@ -6,7 +6,13 @@ int main() {
     p = &i;
     i = 10;
 
-    printf("%d\n", *p);
+    printf("i = %d\n", *p);
+
+    int ar[10] = {12, 24, 36, 48, 60, 72, 84, 96, 108, 120}, *q;
+
+    q = &ar[0];
+
+    printf("ar[6] = %d\n", *(q+6));
 
     return 0;
 }
